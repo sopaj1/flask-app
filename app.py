a6532cd07c0e8dc5e8714e7bb8778eb70a8ef34d
sopaj <sopaj@msoe.edu>
2025-03-03 16:31:53 -0600

    hello world2

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()  # debug=True for development.  Don't use in production!
+    app.run(host="0.0.0.0")  # debug=True for development.  Don't use in production!
